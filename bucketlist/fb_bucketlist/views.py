33765de41963cba8702a21e7a713b93b29155886
Mike Yue <mikeyue97@gmail.com>
2021-12-29 20:06:57 -0800

    Fixed logging location

--- bucketlist/fb_bucketlist/views.py
+++ bucketlist/fb_bucketlist/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from django.http.response import HttpResponse
 import logging
+import os
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
-fh = logging.FileHandler("application_log.log")
+fh = logging.FileHandler(os.getcwd() + "/application_log.log")
 fh.setLevel(logging.INFO)
 LOGGER.addHandler(fh)
 loggingFormat = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -14,6 +15,6 @@ fh.setFormatter(loggingFormat)
 # Create your views here.
 class bucketListBotView(generic.View):
     def get(self, request, *args, **kwargs):
-        LOGGER.info("Reached bucketlistbot Webhook")
+        LOGGER.info("Reached bucketlistbot Webhooks")
         return HttpResponse("Hello World")
 
