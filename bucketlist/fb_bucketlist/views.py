6f416333a0806bd0a5810651adc3913fabd1a9d2
Mike Yue <mikeyue97@gmail.com>
2021-12-30 12:57:45 -0800

    Logging received user message

--- bucketlist/fb_bucketlist/views.py
+++ bucketlist/fb_bucketlist/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http.response import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 import logging
 import os
 
@@ -15,13 +18,21 @@ LOGGER.addHandler(fh)
 loggingFormat = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(loggingFormat)
 
-# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
 class bucketListBotView(generic.View):
     def get(self, request, *args, **kwargs):
-        if self.request.GET.get('hub.verify_token', "") == '0808':
+        if self.request.GET.get('hub.verify_token', "1234") == '0808':
             LOGGER.info("Valid verify token received for webhook")
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             LOGGER.error("Invalid verify token received")
             return HttpResponse('Error, invalid token')
-
+    
+    
+    def post(self, request, *args, **kwargs):
+        incoming_message = json.loads(self.request.body.decode("utf-8"))
+        for entry in incoming_message["entry"]:
+            for message in entry["messaging"]:
+                if "message" in message:
+                    LOGGER.info("Received message: " + message)
+        return HttpResponse()
