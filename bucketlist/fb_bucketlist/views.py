5d6ef554cd087b0da025391c9c4793d830c10543
Mike Yue <mikeyue97@gmail.com>
2021-12-29 19:55:18 -0800

    Added logging formatting

--- bucketlist/fb_bucketlist/views.py
+++ bucketlist/fb_bucketlist/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http.response import HttpResponse
+import logging
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+fh = logging.FileHandler("application_log.log")
+fh.setLevel(logging.INFO)
+LOGGER.addHandler(fh)
+loggingFormat = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(loggingFormat)
 
 # Create your views here.
 class bucketListBotView(generic.View):
     def get(self, request, *args, **kwargs):
+        LOGGER.info("Reached bucketlistbot Webhook")
         return HttpResponse("Hello World")
 
